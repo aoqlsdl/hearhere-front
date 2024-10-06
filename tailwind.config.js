8edd9089734c452832843eb6b8cbcdf0f62b98f5
aoqlsdl <hyeeev@gmail.com>
2024-10-06 20:28:12 +0900

    chore: 폰트 전역설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#2bca43",
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    PINK: "#E24848",
+                    L_BLACK: "#383838",
+                    S_BLACK: "#464444",
+                },
+            },
+            fontFamily: {
+                sans: ["Pretendard-Regular", "sans-serif"], // default font
+                Inter: ["Inter", "sans-serif"],
+            },
         },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
