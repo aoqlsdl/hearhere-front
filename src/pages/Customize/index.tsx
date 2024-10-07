44966f8415bd37fc8da07d3d67ee87f13fcb7ea1
aoqlsdl <hyeeev@gmail.com>
2024-10-08 00:45:47 +0900

    chore: 라우팅을 위한 페이지 컴포넌트 생성

---
+++ src/pages/Customize/index.tsx
@@ -0,0 +1,9 @@
+const Customize = () => {
+    return (
+        <div>
+            <h1>Customize</h1>
+        </div>
+    );
+};
+
+export default Customize;
---
+++ src/pages/Library/index.tsx
@@ -0,0 +1,9 @@
+const Library = () => {
+    return (
+        <div>
+            <h1>My Library</h1>
+        </div>
+    );
+};
+
+export default Library;
