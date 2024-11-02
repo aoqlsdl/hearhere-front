d0c184cc862c13858475674b55bcc334c9fed5ee
aoqlsdl <hyeeev@gmail.com>
2024-11-02 23:02:39 +0900

    chore: 주석 삭제

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,18 +2,14 @@ import { NavLink, useLocation } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userState } from "../../recoil/user/atom";
 import useLogout from "../../hooks/auth/useLogout";
+
 const Navbar = () => {
     const user = useRecoilValue(userState);
     const location = useLocation();
     const logout = useLogout();
 
     return (
-        <nav
-            className="flex justify-between top-0 w-screen h-[4.25rem] fixed z-50"
-            // initial={{ opacity: 0, y: 0 }} // 초기 상태: 투명하고 위로 이동
-            // animate={isOn ? { opacity: 1, y: 0 } : { opacity: 0, y: 0 }} // isOn에 따라 나타남/사라짐
-            // transition={{ duration: 1, ease: "linear" }} // 애니메이션 지속 시간과 전환 설정
-        >
+        <nav className="flex justify-between top-0 w-screen h-[4.25rem] fixed z-50">
             <div className="flex justify-center items-center ml-9">
                 {user.username === null &&
                 (location.pathname === "/login" ||
