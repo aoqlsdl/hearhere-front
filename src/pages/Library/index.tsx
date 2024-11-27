e2105a27e14e62b5e94568bd23f0590ea7b48ca6
aoqlsdl <hyeeev@gmail.com>
2024-11-27 12:59:33 +0900

    feat: library 페이지 api 연결

--- src/pages/Library/index.tsx
+++ src/pages/Library/index.tsx
@@ -1,12 +1,32 @@
 import ASMRList from "../../components/ASMRList";
-import asmrlist from "../../_mock/asmr.json";
 import NewList from "../../components/NewList";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useAsmrList } from "../../hooks/asmr/useAsmrList";
 import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 
 const Library = () => {
+    const { data: asmrList, error } = useAsmrList();
+
+    if (error) {
+        console.log(error);
+    }
+    if (!asmrList || asmrList.length === 0)
+        return (
+            <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                slidesPerView={3}
+                centeredSlides={true}
+                grabCursor={true}
+                spaceBetween={30}
+            >
+                <SwiperSlide>
+                    <NewList key="new-list" />
+                </SwiperSlide>
+            </Swiper>
+        );
     return (
         <>
             <Swiper
@@ -17,10 +37,14 @@ const Library = () => {
                 grabCursor={true}
                 spaceBetween={30}
             >
-                {asmrlist.map((asmr) => (
-                    <SwiperSlide key={asmr.id}>
+                {asmrList.map((asmr) => (
+                    <SwiperSlide key={asmr.asmrId}>
                         <div className="w-full flex items-center">
-                            <ASMRList key={asmr.id} asmr={asmr} />
+                            <ASMRList
+                                key={asmr.asmrId}
+                                asmrTitle={asmr.title}
+                                asmrId={asmr.asmrId}
+                            />
                         </div>
                     </SwiperSlide>
                 ))}
