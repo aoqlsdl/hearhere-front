58b0ab6c95ed8129982cde2900fb6654629d5541
aoqlsdl <hyeeev@gmail.com>
2024-11-27 13:21:50 +0900

    build: vercel 에러를 방지하기 위한 임시방편으로 임의의 변수 생성

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -1,9 +1,17 @@
 import MusicPlayer from "../../components/MusicPlayer";
+import { useState } from "react";
 
 const Player = () => {
+    const asmrData = {
+        asmrId: 0,
+        title: "",
+        musicUrl: "",
+        soundDetails: [],
+    };
+    const [, setIsLoginOn] = useState(false);
     return (
         <div className="overflow-hidden flex items-center justify-center">
-            <MusicPlayer />
+            <MusicPlayer asmrData={asmrData} setIsLoginOn={setIsLoginOn} />
         </div>
     );
 };
