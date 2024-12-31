26d1f905d8d09f81f36b30b2c007ec36bf79f6ea
aoqlsdl <hyeeev@gmail.com>
2025-01-01 07:16:22 +0900

    feat: 커스텀 페이지에 asmr 데이터 받아 렌더링

--- src/components/Custom/index.tsx
+++ src/components/Custom/index.tsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
+import Waveform from "../Waveform";
 
-const Custom = () => {
+interface SoundDetail {
+    soundId: number;
+    url: string;
+    length: string;
+}
+
+interface CustomProps {
+    asmrData: {
+        asmrId: number;
+        title: string;
+        musicUrl: string | null;
+        soundDetails: SoundDetail[];
+    };
+}
+
+const Custom = ({ asmrData }: CustomProps) => {
     const [isPlaying] = useState(false);
     return (
         <div className="flex flex-col">
@@ -24,6 +40,19 @@ const Custom = () => {
                     <span className="ml-[0.44rem] text-base 1440:text-[1.44rem]">Play</span>
                 </button>
             )}
+            {/* <div className=""> */}
+            {asmrData.soundDetails.map((detail) => (
+                <div
+                    key={detail.soundId}
+                    className="bg-slate-50 border-b-2 h-[7.44rem] flex flex-col justify-center p-[1.88rem]"
+                >
+                    <h3 className="mb-2">Track {detail.soundId}</h3>
+                    <Waveform audioUrl={detail.url} />
+
+                    {/* <p>Length: {detail.length}</p> */}
+                </div>
+            ))}
+            {/* </div> */}
             <progress></progress>
         </div>
     );
