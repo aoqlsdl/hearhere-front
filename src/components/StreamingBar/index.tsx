ced2675f522d989301d3b89e7b929507c38b3360
aoqlsdl <hyeeev@gmail.com>
2025-01-05 02:22:40 +0900

    feat: 10초 앞뒤로 이동하는 기능 추가

--- src/components/StreamingBar/index.tsx
+++ src/components/StreamingBar/index.tsx
@@ -50,7 +50,14 @@ const StreamingBar = ({ asmrData }: Props) => {
                 const currentTimes = waveSurferRefs.current.map((ws) => ws.getCurrentTime());
                 const maxCurrentTime = Math.max(...currentTimes);
                 setCurrentTime(maxCurrentTime);
-                setProgress((maxCurrentTime / totalDuration) * 100);
+                const newProgress = (maxCurrentTime / totalDuration) * 100;
+                setProgress(newProgress);
+
+                // 100%가 되면 isPlaying이 false가 되도록 수정
+                if (newProgress >= 100) {
+                    setIsPlaying(false);
+                    waveSurferRefs.current.forEach((waveSurfer) => waveSurfer.pause());
+                }
             });
         });
 
@@ -66,6 +73,15 @@ const StreamingBar = ({ asmrData }: Props) => {
             await Promise.all(waveSurferRefs.current.map((waveSurfer) => waveSurfer.play()));
         }
         setIsPlaying(!isPlaying);
+    };
+
+    const seekToTime = (offset: number) => {
+        const newTime = Math.min(Math.max(0, currentTime + offset), totalDuration); // 제한된 범위로 이동
+        setCurrentTime(newTime);
+
+        const newProgress = newTime / totalDuration;
+        waveSurferRefs.current.forEach((waveSurfer) => waveSurfer.seekTo(newProgress));
+        setProgress(newProgress * 100);
     };
 
     const handleProgressClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -91,18 +107,18 @@ const StreamingBar = ({ asmrData }: Props) => {
                 </div>
             ) : ( */}
             <div>
-                <div className="flex flex-row w-full justify-center items-center mt-[2.48rem]">
+                <div className="flex flex-row w-full max-w-full overflow-hidden justify-center items-center mt-[2.48rem]">
                     {/* 현재 재생 시간 */}
                     <span className="text-[1.25rem] font-light mr-[0.94rem]">
                         {formatTime(currentTime)}
                     </span>
                     <div
-                        className="relative w-[29.44rem] h-1 bg-primary-BLACK300 cursor-pointer flex items-center"
+                        className="relative max-w-[29.44rem] w-[29.44rem] h-1 bg-primary-BLACK300 cursor-pointer flex items-center"
                         onClick={handleProgressClick}
                     >
                         {/* Progress Bar */}
                         <div
-                            className="h-1 w-[29.44rem] absolute top-0 left-0 bg-primary-BLACK300"
+                            className="h-1 absolute top-0 left-0 bg-primary-BLACK300"
                             style={{ width: `${progress}%` }}
                         ></div>
 
@@ -121,7 +137,10 @@ const StreamingBar = ({ asmrData }: Props) => {
                 </div>
                 {/* Play/Pause 버튼 */}
                 <div className="mt-6 flex flex-row w-[13.39rem] justify-between m-auto mb-[3.25rem]">
-                    <button className="bg-forward bg-cover bg-no-repeat w-6 h-6" />
+                    <button
+                        className="bg-forward bg-cover bg-no-repeat w-6 h-6"
+                        onClick={() => seekToTime(-10)}
+                    />
                     <button
                         onClick={() => togglePlayPause()}
                         className="w-[1.81rem] h-[1.81rem] flex items-center justify-center"
@@ -132,7 +151,10 @@ const StreamingBar = ({ asmrData }: Props) => {
                             <img src="/assets/icons/play.svg" alt="Play" />
                         )}
                     </button>
-                    <button className="bg-backward bg-cover bg-no-repeat w-6 h-6" />
+                    <button
+                        className="bg-backward bg-cover bg-no-repeat w-6 h-6"
+                        onClick={() => seekToTime(10)}
+                    />
                 </div>
             </div>
             {/* )} */}
