f8abbd5d963751400ba081f97db7dc5f6ea79702
aoqlsdl <hyeeev@gmail.com>
2025-01-11 23:23:25 +0900

    ui: pretendard 폰트 적용

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
         <div className="w-screen bg-primary-SAND flex justify-center mt-9">
-            <div className="w-[26.25rem] h-[26.25rem] text-center flex justify-center items-center bg-lp bg-cover mt-[7.38rem] mb-[8.88rem] animate-[spin_3s_linear_infinite]">
+            <div className="w-[26.25rem] h-[26.25rem] font-pre_400 text-center flex justify-center items-center bg-lp bg-cover mt-[7.38rem] mb-[8.88rem] animate-[spin_3s_linear_infinite]">
                 <div className="text-[1.2rem] 1440:text-[1.88rem]">Loading...</div>
             </div>
         </div>
